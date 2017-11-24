4c0287046320a15435fb2de0b326bf778c062dd1
kguzel <kguzel@bkm.com.tr>
2017-11-24 10:06:25 +0300

    First init commit

--- FlowSwiftTest/ViewController.swift
+++ FlowSwiftTest/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import BKMExpress
 
-class ViewController: UIViewController {
+let kTICKET_TOKEN:String = "Ticket Token will be given by BKM after the merchant integration"
+let kTICKET_PATH:String = "Ticket Path will be given by BKM after the merchant integration"
+let kTICKET_ID:String = "Ticket Id will be given by BKM after the merchant integration"
+
+class ViewController: UIViewController,BKMExpressPaymentDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        let instanceOfCustomObject: BKMExpressPaymentViewController = BKMExpressPaymentViewController(bexTicketToken: kTICKET_TOKEN, bexTicketPath: kTICKET_PATH, bexTicketId :kTICKET_ID, delegate: self)
+        
+        instanceOfCustomObject.setEnableDebugMode(false)
+        
+        self.present(instanceOfCustomObject , animated:true,completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func bkmExpressPaymentDidComplete() {
+        NSLog("Successful payment")
+    }
+    
+    func bkmExpressPaymentDidFail(_ error: Error!) {
+        NSLog("An error has occurred on payment %@", error.localizedDescription)
+    }
+    
+    func bkmExpressPaymentDidCancel() {
+        NSLog("Payment is cancelled by user")
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FlowSwiftTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FlowSwiftTest
+  
+  use_frameworks!
+  pod 'BKMExpressFlowSDK'
+  
+
+  target 'FlowSwiftTestTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FlowSwiftTestUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
