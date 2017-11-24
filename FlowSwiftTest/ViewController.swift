26b653bd355b4fb22c85cad9c499b8f1c246bf27
kguzel <kguzel@bkm.com.tr>
2017-11-24 08:40:14 +0300

    Initial Commit

---
+++ FlowSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlowSwiftTest
+//
+//  Created by Kadir Guzel on 24/11/2017.
+//  Copyright © 2017 Kadir Guzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
